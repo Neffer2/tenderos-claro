9a16dc64377ff0dd2888e8c744fdd9fe23737a15
Neffer2 <n3f73r@gmail.com>
2022-08-18 17:38:38 -0500

    maqueta ruleta

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>D&iacute;a del tendero</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>
+	<div class="mainbox">
+		<div class="box">
+			<div class="box1">
+				<span class="span1"></span>
+				<span class="span2"></span>
+				<span class="span3"></span>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
